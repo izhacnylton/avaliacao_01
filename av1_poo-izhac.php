421e91c99b1c117782548faeef227eb1cac71683
Izhac Nylton <izhac2006@gmail.com>
2026-02-24 16:50:26 -0400

    primeira avaliacao poo

--- av1_poo-izhac.php
+++ av1_poo-izhac.php
@@ -7,44 +7,67 @@
 </head>
 <body>
     <?php 
-    echo "AVALIAÇÃO 01: Programação Orientada a Objetos <br><br>";
+    //<em> e </em> para italico
+    echo "<strong>AVALIAÇÃO 01</strong>: Programação Orientada a Objetos <br>Criação da Classe <strong><em>Carro</em></strong>: <br><br>";
+    
+    //criação da classe
     class Carro{
-        public $marca;
+        public $marca;  //criando atributos 
         public $modelo;
         public $ano;
         public $cor;
         public $velocidade = 0;
 
+        //criando funcoes acelerar e frear
         public function acelerar(){
             $this -> velocidade += 10;
-            echo "O Carro Acelerou. Velocidade:  " . $this -> velocidade . " Km/h <br>";
+            echo "O Carro Acelerou. Velocidade: <strong> " . $this -> velocidade . "Km/h</strong> <br>";
         }
-
+        //frear tem um condição de se a velocidade for igual ou maior que 10, irá decrementar, para que o valor nao se torne negativo 
         public function frear(){
             if($this -> velocidade >= 10){
                 $this -> velocidade -= 10;
-            }
-            echo "O carro freou. Velocidade: " . $this -> velocidade . " Km/h <br>";
-
+            }// <strong> e </strong> para negrito
+            echo "O carro freou. Velocidade: <strong>" . $this -> velocidade . " Km/h</strong> <br><br>";
         }
-
+        //criando funcoes e dando valores pré-definidos para restantes atributos 
         public function marca(){
-            $this -> marca = 
+            $this -> marca = "Uno";
+            echo "A marca do carro é: " . $this -> marca . "<br>";
         }
 
+        public function modelo(){
+            $this -> modelo = 'Com escada';
+            echo "O modelo do seu carro é: ". $this -> modelo . "<br>";
+        }
+
+        public function ano(){
+            $this -> ano = '1988';
+            echo "O ano do seu carro é: ". $this -> ano . "<br>";
+        }
+
+        public function cor(){
+            $this -> cor = 'verde';
+            echo "A cor do seu carro é: ". $this -> cor . "<br>";
+        }
     }
-
+    //criação do objeto na classe CARRO
     $meuCarro = new Carro();
-    $meuCarro->acelerar();
-    $meuCarro->acelerar();
-    $meuCarro->frear();
 
-    $meuCarro -> marca = 
-    $meuCarro -> modelo = 'Com escada';
-    $meuCarro -> ano = '1988';
-    $meuCarro -> cor = 'verde';
-    
-    echo "Seu carro é um: " . $marca 
+    //$meuCarro->marca = "honda"  <-- como inserir novos valores à atributos;
+
+    //instanciando as funcoes de acelerar e frear
+    $meuCarro -> acelerar();
+    $meuCarro -> acelerar();
+    $meuCarro -> acelerar();
+    $meuCarro -> acelerar();
+    $meuCarro -> frear();
+
+    //instanciando os atributos marca, modelo, ano e cor com valores pré-definidos
+    $meuCarro -> marca();
+    $meuCarro -> modelo();
+    $meuCarro -> ano();
+    $meuCarro -> cor();
 
     
     ?>
